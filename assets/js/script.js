34a2346ee2fcb4ef43736133bc751d5d267e2bd0
Nathan Aramyan <nathan.aramyan@gmail.com>
2022-11-02 20:48:36 -0600

    CSS to make it look good, declared variables in script.js

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var timeRemaining = document.querySelector("#pTimeRemaining");
+var startButton = document.querySelector("#startButton");
+var submitButton = document.querySelector("#submitButton");
+var pQuestion = document.querySelector("#pQuestion");
+var olOptions = document.querySelector("#olOptions");
+var playerName = document.querySelector("#playerName");
+var viewHighScores = document.querySelector("#viewHighScores");
+var pHighScores = document.querySelector("#pHighScores");
+
